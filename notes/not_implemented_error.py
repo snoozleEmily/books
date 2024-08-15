6accab729fac3529bf5eefd830663acf8b95a266
Emily <emilyalvimicm@gmail.com>
2024-08-15 11:15:55 -0300

    Update not_implemented_error.py

--- notes/not_implemented_error.py
+++ notes/not_implemented_error.py
@@ -1,11 +1,12 @@
 def first():
     print('Function first was called')
+    
 def second():
     print('Function second was called')
+    
 def third():
     first()
-    second()
-    
+    second()    
     raise NotImplementedError("You need to implement this function.")
 
 third()
